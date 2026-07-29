9c00eb70246f0cb0b447df3d1e9d69e6ccb90985
kirstenlayters <kirsten.layters@gmail.com>
2026-07-29 10:56:56 +0200

    Add files via upload

---
+++ exercise 3.php
@@ -0,0 +1,38 @@
+<?php 
+echo "My name is Kirsten Layters. My favourite programming language is HTML because it is easy to use, simple and predictable.";
+?>
+
+<br><br>
+
+<?php
+$a = 12;
+$b = 8;
+echo $a + $b;
+?>git add "Exercise 3/"
+
+<br><br>
+
+<?php
+echo date("l, F j, Y");
+?>
+
+<br><br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercise 1</title>
+</head>
+<body>
+    <h1>
+        <?php echo "Welcome to PHP Programming!"; ?>
+    </h1>
+</body>
+</html>
+
+<br><br>
+
+<?php 
+echo "Your lucky number today is: " . rand(1, 100) . ".";
+?>
